091834a71f64ae0b4ca8881dff18ccf34ea33167
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-09-30 19:48:55 +0900

    Update: UIStackView
    UIStackView를 공부하였습니다.

---
+++ Calculator/Calculator/UISatckView.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//UIStackView? -> 열 또는 행에 View 들의 묶음을 배치할 수 있는 간소화된 인터페이스
+
+// Axis -> 방향을 정하는 속성
+// Distribution -> 뷰들의 사이즈를 어떻게 분재할지 설정하는 속성
+// Alignment -> subView들을 어떻게 정렬할지 결정하는 속성
+// Spacing -> 뷰들의 간격을 조정하는 속성
