0c893fbc51c983b7e3eb72b624f69257cccef4ad
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-11-24 16:01:10 +0900

    Add: addWriteDairyView
    WriteDairyView를 만들었습니다.

---
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by DGSW on 2021/11/24.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
