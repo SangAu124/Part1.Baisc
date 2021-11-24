c8ccc850c990a9d587b136427c8002e6d0aa2056
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-11-25 08:45:02 +0900

    Add: addUploadFunction
    UploadFunction을 만들었는데... 버그가...

---
+++ Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by DGSW on 2021/11/24.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
