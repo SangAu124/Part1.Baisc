e8caef2d801e8acf9d97967f87e64e5b61da4361
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-11-24 19:52:47 +0900

    Add: addDateWheels
    DateWheels와 작은 버그를 수정하였습니다.

--- Diary/Diary/WriteDiaryViewController.swift
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -9,9 +9,48 @@ import UIKit
 
 class WriteDiaryViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
+    private let datePicker = UIDatePicker()
+    private var diaryDate: Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureContentsTextView()
+        configureDatePicker()
     }
-
+    
+    private func configureContentsTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        self.contentsTextView.layer.borderColor = borderColor.cgColor
+        self.contentsTextView.layer.borderWidth = 0.6
+        self.contentsTextView.layer.cornerRadius = 6.0
+    }
+    
+    private func configureDatePicker() {
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChanged(_:)), for: .valueChanged)
+        self.datePicker.locale = Locale(identifier: "ko-KR")
+        self.dateTextField.inputView = self.datePicker
+    }
+    
+    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @objc private func datePickerValueDidChanged(_ datePicker: UIDatePicker){
+        let formmater = DateFormatter()
+        formmater.dateFormat = "yyyy년 MM월 dd일(EEEEE)"
+        formmater.locale = Locale(identifier: "ko_KR")
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formmater.string(from: datePicker.date)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
