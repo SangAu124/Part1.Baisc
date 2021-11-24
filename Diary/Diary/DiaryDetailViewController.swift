fc45a7a23a78da0a2f6a3cd9b042c48fe0885a49
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-11-24 16:10:55 +0900

    Add: addDiaryDetailView
    DiaryDetailView를 만들었습니다.

---
+++ Diary/Diary/DiaryDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by DGSW on 2021/11/24.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
