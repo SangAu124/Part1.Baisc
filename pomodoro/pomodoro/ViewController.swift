885cd3e6ee58f7ba0ee920dab448c904ab5cf770
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-11-27 23:11:08 +0900

    .
    .

--- pomodoro/pomodoro/ViewController.swift
+++ pomodoro/pomodoro/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var toggleButton: UIButton!
+    
+    var duration = 60
+    var timerStatus: TimerStatus = .end
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func tabCancelButton(_ sender: UIButton) {
+    }
+    @IBAction func tabToggleButton(_ sender: UIButton) {
+        self.duration = Int(self.datePicker.countDownDuration) // countDownDuration = 입력받은 시간을 초로 표현 해준다.
+        
+        switch self.timerStatus{
+        case .end:
+            self.timerStatus = .start
+            
+        default:
+        }
+    }
 }
 
